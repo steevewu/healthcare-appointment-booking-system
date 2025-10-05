20d2b20963c807cbe6e3d32c1e39a904dd6dc638
ergou <nqaisme@icloud.com>
2025-10-05 21:35:12 +0700

    ft: departs mng

---
+++ lang/vendor/filament/vi/resources.php
@@ -0,0 +1,24 @@
+<?php
+
+
+return [
+
+
+
+    'departments' => [
+        'label' => 'Khoa',
+        'plural_label' => 'Khoa',
+        'navigation_label' => 'Quản lý khoa',
+        'group' => 'Hehe'
+    ],
+
+
+
+    'doctors' => [
+
+    ],
+
+    ''
+    
+    
+];
